c8809f4d83ee7455706395eb5f2afafbf5c4bb7c
bmuradova17974 <Bmuradova17974@ada.edu.az>
2024-04-10 13:53:30 +0400

    adding MainFrame.java file

---
+++ MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MainFrame{
+    public static void main(String[] args) {
+      new MyFrame();
+      JLabel jlabel = new JLabel();
+    }
+}
